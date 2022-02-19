4d9a0d4d93d19e27f86fdaaf82118f5921980c0b
Federico Pregnolato <fed.pregnolato@gmail.com>
2022-02-19 22:53:00 +0100

    recipe list completed

---
+++ pages/recipes/index.js
@@ -0,0 +1,37 @@
+import { Client } from '@notionhq/client';
+import Link from 'next/link';
+import slugify from 'slugify';
+
+const RecipeList = ({ recipes }) => {
+    return recipes.map((recipes) => (
+            <p key={recipes}>
+                <Link href={`/recipes/${slugify(recipes).toLowerCase()}`}>
+                    <a>{recipes}</a>
+                </Link>
+            </p>
+        ));
+};
+
+export const getStaticProps = async () => {
+    const notion = new Client({
+        auth: process.env.NOTION_SECRET,
+    })
+    const data = await notion.blocks.children.list({
+        block_id: process.env.PAGE_ID,
+    })
+
+    const recipes = []
+    data.results.map(result => {
+        if (result.type === 'child_page') {
+            recipes.push(result.child_page.title)
+        }
+    })
+
+    return {
+        props:{
+            recipes,
+        }
+    }
+};
+
+export default RecipeList;
